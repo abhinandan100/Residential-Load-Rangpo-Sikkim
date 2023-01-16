99002318673c1c5e87c2119cb823a6ef685f4f0b
abhinandan100 <80942390+abhinandan100@users.noreply.github.com>
2023-01-16 22:45:20 +0530

    Add files via upload

---
+++ Category 1/Category 1 Sub Category 2/Minimum/Weekdays/Mar1.py
@@ -0,0 +1,14 @@
+#One-Room Minimum Connected Load
+#Create new user classes
+OR = User("One room minimal",1,1)
+User_list.append(OR)
+
+#One-Room Minimum Connected Load
+OR_indoor_LEDlight1 = OR.Appliance(OR,1,7,2,420,0.15,15)
+OR_indoor_LEDlight1.windows([1020,1320],[240,330],0.1)
+OR_lavatory_LEDBulb = OR.Appliance(OR,1,5,1,60,0.2,15)
+OR_lavatory_LEDBulb.windows([870,1440],[0,360],0.1)
+OR_Phone_charger = OR.Appliance(OR,4,5,2,120,0.3,15)
+OR_MosquitoRepellent = OR.Appliance(OR,1,7,2,480,0.15,30)
+OR_MosquitoRepellent.windows([1200,1440],[0,360],0.35)
+
